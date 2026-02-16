cdef8001948971c7519d96c4c362df8cedf076c8
bekyamen <bereket1terefe@gmail.com>
2026-02-16 18:17:16 +0300

    update api again

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 if (!API_BASE) {
   throw new Error('API base URL is not defined in environment variables');
